a4a3f4d0b8e283fc9a4143bea124d008813aaa3e
Carson Black <cbphoto720@gmail.com>
2022-01-07 14:15:50 -0800

    add datalog capability Testcars

--- matlab/Testcars.m
+++ matlab/Testcars.m
@@ -9,14 +9,9 @@ fclose(fid);
 num_tags=length(tagID);
 clear C fid ans
 
-% Configure Serial & save location
+% Configure Serial
 Antenna=serialport(COM,9600);
 configureTerminator(Antenna,93); % 93 ascii ]
-% 1/6/22 Make data directory and save interpreted serial to it
-% if [status,~]=mkdir('data')
-% else
-%     mkdir data
-% end
 
 % Read Serial
 a=readline(Antenna);
@@ -29,3 +24,70 @@ index = A(2) - tagID(1) + 1; %matlab arrays are one based,not zero based
 if num_elements==2 && index <= num_tags && index > 0
     disp('READ!')
 end
+
+%% Create Data folder and datalog
+
+% Create data folder, specify filename & path
+if ~isfolder('data') %check if data dir exists
+    mkdir('data')
+end
+dtstr=datestr(datetime('now'),'mm-dd-yy'); %get current date
+filename=['datalog','_',dtstr,'.txt'];
+filename=convertCharsToStrings(filename); %convert to string
+outpath=fullfile('data',filename);
+
+% open datalog
+[fid, msg]=fopen(outpath,'a');
+if fid < 0
+  error('Failed to open file "%s" because: "%s"', filename, msg);
+end
+
+% write datait
+timestamp=datestr(datetime('now'),30); %'yyyymmddTHHMMSS' (ISO 8601)
+output=[newline,timestamp];
+writecount=fwrite(fid,output);
+
+% close datalog
+msg=fclose(fid);
+if msg < 0
+  error('Failed to close file "%s"', filename);
+end
+clear fid msg dtstr
+
+%% Write table method
+
+% Create data table
+timestamp={datestr(datetime('now'),30)}; %'yyyymmddTHHMMSS' (ISO 8601);
+tagID={nan};
+RSI=tagID;
+GPS=tagID;
+heading=tagID;
+flag=logical(0);
+T=table(timestamp,tagID,RSI,GPS,heading,flag);
+
+% write data table
+timestamp=datestr(datetime('now'),30); %'yyyymmddTHHMMSS' (ISO 8601)
+% [tagID,RSI]= 
+GPS=nan;
+heading=nan;
+flag=logical(0);
+
+%% Read data from datalog
+
+% open datalog
+fid = fopen(outpath,'r');
+if fid < 0
+  error('Failed to open file "%s" because: "%s"', filename, msg);
+end
+
+% read datalog
+test=textscan(fid,'%s', 'Delimiter', '');
+test=test{1};
+test2=sscanf(test{end},'%8dT%6d',2)
+
+%close datalog
+msg=fclose(fid);
+if msg < 0
+  error('Failed to close file "%s"', filename);
+end
+clear fid msg dtstr
