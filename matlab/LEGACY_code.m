1876098610c76dd295f755ea99f47232dbf2520d
cbphoto720 <cbphoto720@gmail.com>
2022-01-20 14:11:24 -0800

    finder functions

--- matlab/LEGACY_code.m
+++ matlab/LEGACY_code.m
@@ -44,3 +44,14 @@ toc
 % staments was 0.003235 s..  
 % the speed discrepancy only increases when reading multiple lines of data
 %% ________________________________________________________________________
+% Find numbers in range
+
+r = [1, 10];
+xRange = [5 2 1 -1 5 67 3];
+ii = xRange >= r(1) & xRange <= r(2)
+out = xRange;
+out(~ii) = nan;
+
+% Find a struct field
+a.b.c = 1;
+isfield(a.b,'c')
