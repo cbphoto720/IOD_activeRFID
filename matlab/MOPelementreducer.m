f68245c36406d48bef1fc82190a2aca77f46cb73
cbphoto720 <cbphoto720@gmail.com>
2022-01-24 12:22:13 -0800

    Create MOPelementreducer.m

---
+++ matlab/MOPelementreducer.m
@@ -0,0 +1,28 @@
+function [latcord,loncord] = MOPelementreducer(MOPkmlpath)
+%convert a Mmop.kml into a single graphical element for speed
+%plotting.
+%{   
+    Accomplished by inserting NaN values between line coordinates in order
+    to complete independant lines into 1 graphic element to increase
+    responsivness.
+
+    Input:
+        - MOPkmlpath - file path to MOP.kml
+
+    Output:
+        - Latcord - Latitude lines matrix seperated by NaN values
+        - Loncord - Longitude lines matrix seperated by NaN values
+%}
+importkml=kml2struct(MOPkmlpath);
+iii=strcmp({importkml.Geometry}, 'Line')==1; % remove excess points
+MOPkml=importkml(iii);
+
+% Convert struct to matrix for graphing
+latcord=[MOPkml.Lon];
+latcord=[latcord;nan(1,size(latcord,2))];
+latcord=latcord(:);
+
+loncord=[MOPkml.Lat];
+loncord=[loncord;nan(1,size(loncord,2))];
+loncord=loncord(:);
+end
